41bffb7f84ae3a071c812e5bd11d45e456a3eea7
iceanddust <iceanddust@proton.me>
2026-02-20 11:26:17 -0300

    Fix import

--- src/arbitrum-polling.ts
+++ src/arbitrum-polling.ts
@@ -1,52 +1,56 @@
 import {
-    Voted
-  } from '../generated/PollingEmitter/PollingEmitter';
-  import { ArbitrumPollVote, ArbitrumPoll, ArbitrumVoter } from '../generated/schema';
-  import { BIGINT_ZERO } from './helpers/constants';
-  import { PollCreated } from '../generated/PollingEmitterArbitrum/PollingEmitter';
+  ArbitrumPollVote,
+  ArbitrumPoll,
+  ArbitrumVoter,
+} from '../generated/schema';
+import { BIGINT_ZERO } from './helpers/constants';
+import {
+  PollCreated,
+  Voted,
+} from '../generated/PollingEmitterArbitrum/PollingEmitter';
 
-  // Helper function to get or create an ArbitrumVoter
-  function getArbitrumVoter(address: string): ArbitrumVoter {
-    let voter = ArbitrumVoter.load(address);
-    if (!voter) {
-      voter = new ArbitrumVoter(address);
-      voter.numberPollVotes = 0;
-      voter.lastVotedTimestamp = BIGINT_ZERO;
-    }
-    return voter;
+// Helper function to get or create an ArbitrumVoter
+function getArbitrumVoter(address: string): ArbitrumVoter {
+  let voter = ArbitrumVoter.load(address);
+  if (!voter) {
+    voter = new ArbitrumVoter(address);
+    voter.numberPollVotes = 0;
+    voter.lastVotedTimestamp = BIGINT_ZERO;
+  }
+  return voter;
+}
+
+export function handlePollVote(event: Voted): void {
+  const sender = event.params.voter.toHexString();
+  const pollId = event.params.pollId.toString();
+  const optionId = event.params.optionId;
+
+  const voter = getArbitrumVoter(sender);
+  voter.lastVotedTimestamp = event.block.timestamp;
+
+  const voteId = `${pollId}-${sender}-${event.block.number}`;
+
+  let pollVote = ArbitrumPollVote.load(voteId);
+  if (!pollVote) {
+    pollVote = new ArbitrumPollVote(voteId);
+    voter.numberPollVotes = voter.numberPollVotes + 1;
   }
 
-  export function handlePollVote(event: Voted): void {
-    const sender = event.params.voter.toHexString();
-    const pollId = event.params.pollId.toString();
-    const optionId = event.params.optionId;
+  let poll = ArbitrumPoll.load(pollId);
+  if (!poll) {
+    poll = new ArbitrumPoll(pollId);
+    poll.save();
+  }
 
-    const voter = getArbitrumVoter(sender);
-    voter.lastVotedTimestamp = event.block.timestamp;
+  pollVote.voter = voter.id;
+  pollVote.poll = poll.id;
+  pollVote.choice = optionId;
+  pollVote.block = event.block.number;
+  pollVote.blockTime = event.block.timestamp;
+  pollVote.txnHash = event.transaction.hash.toHexString();
+  pollVote.save();
 
-    const voteId = `${pollId}-${sender}-${event.block.number}`;
-
-    let pollVote = ArbitrumPollVote.load(voteId);
-    if (!pollVote) {
-      pollVote = new ArbitrumPollVote(voteId);
-      voter.numberPollVotes = voter.numberPollVotes + 1;
-    }
-
-    let poll = ArbitrumPoll.load(pollId);
-    if (!poll) {
-      poll = new ArbitrumPoll(pollId);
-      poll.save();
-    }
-
-    pollVote.voter = voter.id;
-    pollVote.poll = poll.id;
-    pollVote.choice = optionId;
-    pollVote.block = event.block.number;
-    pollVote.blockTime = event.block.timestamp;
-    pollVote.txnHash = event.transaction.hash.toHexString();
-    pollVote.save();
-
-    voter.save();
+  voter.save();
 }
 
 export function handlePollCreated(event: PollCreated): void {
